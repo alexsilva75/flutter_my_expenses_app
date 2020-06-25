653ae65648f621d454cf4509a08e33de33e07372
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-06-25 10:32:51 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Text(
+              'My Chart!',
+            ),
+          ),
+           Card(
+            child: Text(
+              'Transactions!',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
